d79f28af8a58a2a6f0faa5dd3fb773304c98cd11
Matt Cliff <matt@denvercliffs.com>
2018-07-15 13:38:35 -0600

    updated build/gen scripts

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 # requires docker to be installed
 
 docker run -it --rm  --name travel-maven-build \
-   -u ${UID}\
    -v ${PWD}/out:/usr/src/mymaven \
    -w /usr/src/mymaven \
    maven:3.3-jdk-8 mvn package
+
+# this wasn't working with the gen.sh script
+#   -u ${UID}\
 
--- gen.sh
+++ gen.sh
@@ -3,23 +3,26 @@
 # expects swagger.yaml in current directory
 # new output directory out is generated (this should be excluded from git)
 
+
 docker run -it --rm  --name travel-swagger-codegen \
-   -u ${UID} \
    -v ${PWD}/out:/swagger-api/out \
    -v ${PWD}:/swagger-api/yaml \
    jimschubert/swagger-codegen-cli generate \
    -i /swagger-api/yaml/swagger.yaml \
-   --group-id com.cliffconsulting.travel \
-   --artifact-id spring-travel-api \
-   --artifact-version 1.0.0 \
+   -c /swagger-api/yaml/swagger_opts.json \
    -l spring \
    -o /swagger-api/out
 
+# for some reason having the -u ${UID} flag wasn't working so out/ resource get written as root
 
+
+#   --artifact-id spring-travel-api \
+#   --artifact-version 1.0.0 \
 
 #   --api-package com.cliffconsulting.travel.api \
 #   --model-package com.cliffconsulting.travel.model \
 #   --invoker-package com.cliffconsulting.travel.invoker \
+#   --group-id com.cliffconsulting.travel \
 
 
 #java -jar swagger-codegen-cli.jar generate \
