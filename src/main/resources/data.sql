1ed09a04d0af3b67edf149032d02ce8babffc424
Matt Cliff <matt@denvercliffs.com>
2018-07-16 11:03:27 -0600

    have auto-load working

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-insert into hotel ( hotel_id, name, address, city, phone, stars) values (1, 'The Landmark', '123 South Main', 'Denver', '(303) 555-STAY', 4)
+insert into hotel ( hotel_id, hotel_nm, address, city, phone, stars) values (1, 'The Landmark', '123 South Main', 'Denver', '(303) 555-STAY', 4)
