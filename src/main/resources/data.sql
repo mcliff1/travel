87979aaf0bdb9346dd372624a78dcf4de4bc0ae9
Matt Cliff <matt@denvercliffs.com>
2018-07-15 22:12:14 -0600

    wired in H2 memory database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into hotel
+values (1, 'The Landmark', '123 South Main', 'Denver', '(303) 555-STAY', 4)
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,49 @@
+create table hotel
+(
+  hotel_id integer not null,
+  name varchar(255) not null,
+  address varchar(255),
+  city varchar(255),
+  phone varchar(16),
+  stars integer,
+  primary key(hotel_id)
+)
+
+create table room
+(
+  room_id integer not null,
+  hotel_id integer not null,
+  name varchar(255) not null,
+  address varchar(255),
+  city varchar(255),
+  phone varchar(16),
+  stars integer,
+  primary key(room_id, hotel_id)
+)
+
+create table roomphoto
+(
+  room_id integer not null,
+  url varchar(255) not null
+)
+
+create table reservation
+(
+  reservation_id integer not null,
+  room_id integer not null,
+  start_date varchar(255) not null,
+  end_date varchar(255),
+  num_guests integer,
+  primary key(reservation_id)
+)
+
+create table reservationguest
+(
+  guest_id integer not null,
+  reservation_id integer not null,
+  name varchar(255) not null,
+  age integer,
+  primary key(guest_id)
+)
+
+
