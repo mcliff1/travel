fe53116e837e07763d1bd7e341df0dc5ef5ed7d3
Matt Cliff <matt@denvercliffs.com>
2018-07-17 23:46:56 -0600

    added available room repo

--- src/test/java/com/cliffconsulting/travel/service/HotelServiceTest.java
+++ src/test/java/com/cliffconsulting/travel/service/HotelServiceTest.java
@@ -1,6 +1,7 @@
 package com.cliffconsulting.travel.service;
 
 import com.cliffconsulting.travel.entity.HotelRepository;
+import com.cliffconsulting.travel.model.Hotel;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -12,8 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {HotelService.class})
@@ -22,22 +27,58 @@ public class HotelServiceTest {
     @MockBean
     HotelRepository hotelRepo;
 
-    //@Autowired
-    //HotelService hotelService;
-    HotelService hotelService = new HotelService();
+    @Autowired
+    HotelService hotelService;
 
+    
+	private Hotel mockHotel;
+	private Hotel mockHotel2;
+
+	private String mockHotelJson;
+	private String mockHotelJson2;
+	
+	public HotelServiceTest( ) {
+		mockHotel = new Hotel();
+		mockHotel.setHotelId(99L);
+		mockHotel.setName("Mocktel");
+		mockHotel.setAddress("123 Funny Farm");
+		mockHotel.setCity("Cambridge");;
+		mockHotel.setPhone("(212) 123-AWAY");
+		mockHotel.setStars(4);
+
+		mockHotel2 = new Hotel();
+		mockHotel2.setHotelId(100L);
+		mockHotel2.setName("Mocktel2");
+		mockHotel2.setAddress("6 Sad Street");
+		mockHotel2.setCity("Boston");;
+		mockHotel2.setPhone("(954) WAY-5667");
+		mockHotel2.setStars(2);
+
+		
+		mockHotelJson = "{\"name\" : \"Mocktel\", "
+				+ "\"hotelId\" : 99, "
+				+ "\"address\" : \"123 Funny Farm\", "
+				+ "\"city\" : \"Cambridge\", "
+				+ "\"phone\" : \"(212) 123-AWAY\", "
+				+ "\"stars\" : 4"
+				+ "}";
+
+		mockHotelJson2 = "{\"name\" : \"Mocktel2\", "
+				+ "\"hotelId\" : 100, "
+				+ "\"address\" : \"6 Sad Street\", "
+				+ "\"city\" : \"Boston\", "
+				+ "\"phone\" : \"(954) WAY-5667\", "
+				+ "\"stars\" : 2"
+				+ "}";
+	}    
+    
     @Test
     public void testHaveAMockRepo() {
         assertNotNull(hotelRepo);
     }
 
-//    @Test
-//    public void testFindTheGreatestFromAllData_ForOneValue() {
-//        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {
-//            15
-//        });
-//        assertEquals(15, hotelService.findTheGreatestFromAllData());
-//    }
+	
+    
 
 //    @Test
 //    public void testFindTheGreatestFromAllData_NoValues() {
