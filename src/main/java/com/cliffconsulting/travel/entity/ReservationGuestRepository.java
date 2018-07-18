5f55dbb183955e788e82d52b30a8259e8058013b
Matt Cliff <matt@denvercliffs.com>
2018-07-18 01:29:19 -0600

    updates for reservation

--- src/main/java/com/cliffconsulting/travel/entity/ReservationGuestRepository.java
+++ src/main/java/com/cliffconsulting/travel/entity/ReservationGuestRepository.java
@@ -1,11 +1,22 @@
 package com.cliffconsulting.travel.entity;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ReservationGuestRepository extends JpaRepository<ReservationGuest, Long>{
 
+	@Query("select g from ReservationGuest g where reservation_id = :reservation_id")
+    List<ReservationGuest> findGuestsByReservationId(@Param("reservation_id") Long reservationId);
 
+	@Transactional
+	@Query("delete from ReservationGuest where reservation_id = :reservation_id")
+	void deleteByReservationId(@Param("reservation_id") Long reservationId);
+	
 }
 
--- src/main/java/com/cliffconsulting/travel/service/ReservationService.java
+++ src/main/java/com/cliffconsulting/travel/service/ReservationService.java
@@ -1,6 +1,9 @@
 package com.cliffconsulting.travel.service;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.cliffconsulting.travel.entity.ReservationGuestRepository;
 import com.cliffconsulting.travel.entity.ReservationRepository;
 import com.cliffconsulting.travel.model.Reservation;
+import com.cliffconsulting.travel.model.ReservationGuests;
 
 @Service
 public class ReservationService { 
@@ -27,8 +31,15 @@ public class ReservationService {
         Reservation reservation = new Reservation();
         BeanUtils.copyProperties(reservationDO, reservation);
         
-        
-        
+        List<ReservationGuests> guestList = new ArrayList<ReservationGuests>();
+        List<com.cliffconsulting.travel.entity.ReservationGuest> guestsDO = guestRepo.findGuestsByReservationId(reservationId);
+        guestsDO.stream()
+        	.forEach(guestDO -> {
+        		ReservationGuests guest = new ReservationGuests();
+        		BeanUtils.copyProperties(guestDO,  guest);
+        		guestList.add(guest);
+        	});
+        reservation.setGuests(guestList);
         return reservation;
     }
 
@@ -39,11 +50,24 @@ public class ReservationService {
      */
     public Reservation addReservation(Reservation reservation) {
     	
-    	
         com.cliffconsulting.travel.entity.Reservation reservationDO = new com.cliffconsulting.travel.entity.Reservation();
         BeanUtils.copyProperties(reservation, reservationDO);
         reservationDO = repo.save(reservationDO);
         BeanUtils.copyProperties(reservationDO, reservation);
+
+        //List<com.cliffconsulting.travel.entity.ReservationGuest> guestsDO = guestRepo.findGuestsByReservationId(reservationId);
+        List<ReservationGuests> guestList = new ArrayList<ReservationGuests>();
+        reservation.getGuests().stream()
+        	.forEach(guest -> {
+        		com.cliffconsulting.travel.entity.ReservationGuest guestDO = new com.cliffconsulting.travel.entity.ReservationGuest();
+        		//ReservationGuests guest = new ReservationGuests();
+        		BeanUtils.copyProperties(guest,  guestDO);
+        		guestDO = guestRepo.save(guestDO);
+        		BeanUtils.copyProperties(guestDO, guest);
+        		guestList.add(guest);
+        	});
+        reservation.setGuests(guestList);
+        
         return reservation;
     }
 
@@ -52,10 +76,19 @@ public class ReservationService {
         com.cliffconsulting.travel.entity.Reservation reservationDO = new com.cliffconsulting.travel.entity.Reservation();
         BeanUtils.copyProperties(reservation, reservationDO);
         repo.save(reservationDO);
+        
+        reservation.getGuests().stream()
+        	.forEach(guest -> {
+        		com.cliffconsulting.travel.entity.ReservationGuest guestDO = new com.cliffconsulting.travel.entity.ReservationGuest();
+        		BeanUtils.copyProperties(guest,  guestDO);
+        		guestDO = guestRepo.save(guestDO);
+        	});
     }
 
     public void deleteReservation(long reservationId) {
         repo.deleteById(reservationId);
+        
+        
     }
 
 }
