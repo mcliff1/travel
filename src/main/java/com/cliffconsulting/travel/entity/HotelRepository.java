17ac524aa8e3195a8d1e85022d6937885897da3d
Matt Cliff <matt@denvercliffs.com>
2018-07-22 10:17:26 -0600

    update to get unit test going

--- src/main/java/com/cliffconsulting/travel/entity/HotelRepository.java
+++ src/main/java/com/cliffconsulting/travel/entity/HotelRepository.java
@@ -11,7 +11,7 @@ import com.cliffconsulting.travel.entity.bean.Hotel;
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 
 	// TODO - why did we need to add nativeQuery?
-	//@Query("SELECT h from hotel h where h.hotel_nm = :name")
+	//@Query("SELECT h from hotel h where h.hotel_nm = :name"			)
 	@Query(value = "SELECT h.* from hotel h where h.hotel_nm = :name", nativeQuery = true)
     Hotel findByName(@Param("name") String name);
 }
